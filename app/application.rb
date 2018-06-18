0f1ba320ed06f81461399eb2cdb361d07e909cd7
Pamela Frieze <pfrieze@gmail.com>
2018-06-18 21:33:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,6 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.any?
-      binding.pry
       if true
         @@cart.each do |item|
         resp.write "#{item}\n"
