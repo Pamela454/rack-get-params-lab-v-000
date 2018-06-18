890e5ffc9d717c8582df05af7c341cf835d6cd19
Pamela Frieze <pfrieze@gmail.com>
2018-06-18 21:19:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else req.path.match(/cart/)
+    elsif req.path.match(/cart/)
       @@cart.any?
       if true
         @@carts.each do |item|
