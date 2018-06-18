6ab44d41eb9a652affacd6ee9788eb338607fe61
Pamela Frieze <pfrieze@gmail.com>
2018-06-18 21:14:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,7 @@ class Application
       @@cart == []
       resp.write "Your cart is empty"
       end
+    end
     #elsif req.path.match(/add/)
     #  search_term = req.params["item"]
     #  if @@cart.@@items.detect {|item|
